c63b09d5267a89b6ee42a623f488d878fee362fe
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-06-15 11:03:12 +0800

    Add config file

---
+++ config/config.js
@@ -0,0 +1,8 @@
+const config = {
+    env: process.env.NODE_ENV || 'development',
+    port: process.env.PORT || 3000,
+    jwtSecret: process.env.JWT_SECRET || 'A very long secret key',
+    mongoUri: process.env.MONGODB_URI || process.env.MONGO_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mern_skeleton'
+}
+
+export default config;
