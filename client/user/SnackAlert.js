0f8686d5a2d8ace87f59685a3ff3ddb0c6e75528
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-06-22 21:15:57 +0800

    Add Snack to display errors

---
+++ client/user/SnackAlert.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import MuiAlert from '@material-ui/lab/Alert';
+import Snackbar from '@material-ui/core/Snackbar';
+
+function SnackAlert(props) {
+    return (
+        <Snackbar
+            anchorOrigin={{ vertical: 'bottom', horizontal: 'left' }}
+            open={props.open}
+            onClose={props.handleCloseSnack}
+            autoHideDuration={6000}
+        >
+            <Alert onClose={props.handleCloseSnack} severity={props.severity}>
+                {props.message}
+            </Alert>
+        </Snackbar>
+    )
+}
+
+function Alert(props) {
+    return <MuiAlert elevation={6} variant='filled' {...props} />
+}
+
+export default SnackAlert;
