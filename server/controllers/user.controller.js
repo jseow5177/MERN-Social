4ca8fee77688183bdd656653dd9f2fc8a2e5d0bb
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-06-23 14:59:06 +0800

    Add functions to follow and unfollow users

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,6 +1,9 @@
 import User from '../models/user.model';
 import extend from 'lodash/extend';
 import errorHandler from '../helpers/dbErrorHandler'; // Helper to respond to route requests when error occurs
+import formidable from 'formidable'; // To parse form data and file uploads
+import fs from 'fs';
+import profileImage from './../../client/assets/images/profile-pic.png';
 
 const create = async (req, res, next) => {
     const user = new User(req.body);
@@ -28,8 +31,12 @@ const list = async (req, res) => {
 }
 
 const userById = async (req, res, next, id) => {
+
     try {
-        const user = await User.findById(id);
+        const user = await User.findById(id)
+            .populate('following', '_id name')
+            .populate('followers', '_id name')
+            .exec();
         if (!user) {
             return res.status(400).json({
                 error: 'User not found'
@@ -50,18 +57,39 @@ const read = async (req, res, next) => {
 }
 
 const update = async (req, res, next) => {
-    try {
+
+    const form = new formidable.IncomingForm();
+    form.keepExtensions = true; // Include extensions of original file
+    form.parse(req, async (err, fields, files) => {
+        if (err) {
+            console.log(err);
+            return res.status(400).json({ error: 'Photo cannot be uploaded' });
+        }
+        /*
+            fields: The typical req.body form data
+            files: The file data
+        */
         const user = req.profile;
-        const updatedUser = extend(user, req.body); // req.body contains updated info of user
+        const updatedUser = extend(user, fields);
         updatedUser.updated = Date.now(); // To reflect the latest updated timestamp
-        await updatedUser.save();
-        return res.status(200).json(updatedUser);
-    } catch (err) {
-        console.log(err);
-        return res.status(400).json({
-            error: errorHandler.getErrorMessage(err)
-        });
-    }
+
+        if (files.photo) {
+            updatedUser.photo.data = fs.readFileSync(files.photo.path);
+            updatedUser.photo.contentType = files.photo.type;
+        }
+
+        try {
+            await updatedUser.save();
+            updatedUser.hashedPassword = undefined; // Remove sensitive info
+            return res.status(200).json(updatedUser);
+        } catch (err) {
+            console.log(err);
+            return res.status(400).json({
+                error: errorHandler.getErrorMessage(err)
+            });
+        }
+    });
+
 }
 
 const remove = async (req, res, next) => {
@@ -71,7 +99,7 @@ const remove = async (req, res, next) => {
         return res.status(200).json({ message: 'User successfully removed' });
     } catch (err) {
         return res.status(500).json({
-            error: 'Could not delete user'
+            error: 'Could not delete. Please try again later.'
         })
     }
 }
@@ -107,4 +135,100 @@ const updatePassword = async (req, res, next) => {
     }
 }
 
-export default { create, list, userById, read, update, remove, updatePassword };
+const photo = (req, res, next) => {
+    if (req.profile.photo.data) {
+        res.set('Content-Type', req.profile.photo.contentType);
+        return res.status(200).send(req.profile.photo.data);
+    }
+    next();
+}
+
+const defaultPhoto = (req, res, next) => {
+    // profileImage is compiled and placed in /dist
+    // process.cwd() is the current working directory (/mern-skeleton)
+    return res.status(200).sendFile(process.cwd() + profileImage);
+}
+
+const addFollower = async (req, res, next) => {
+    try {
+        const result = await User.findByIdAndUpdate(req.body.followId, { $push: { followers: req.body.userId } }, { new: true })
+            .populate('following', '_id name')
+            .populate('followers', '_id name')
+            .exec();
+        result.hashedPassword = undefined;
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Fail to follow. Please try again later.'
+        });
+    }
+}
+
+const addFollowing = async (req, res, next) => {
+    try {
+        await User.findByIdAndUpdate(req.body.userId, { $push: { following: req.body.followId } });
+        next();
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Fail to follow. Please try again later.'
+        });
+    }
+}
+
+const removeFollower = async (req, res, next) => {
+    try {
+        const result = await User.findByIdAndUpdate(req.body.unfollowId, { $pull: { followers: req.body.userId } }, { new: true })
+            .populate('following', '_id name')
+            .populate('followers', '_id name')
+            .exec();
+        result.hashedPassword = undefined;
+        return res.status(200).json(result);
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Fail to unfollow. Please try again later.'
+        })
+    }
+}
+
+const removeFollowing = async (req, res, next) => {
+    try {
+        await User.findByIdAndUpdate(req.body.userId, { $pull: { following: req.body.unfollowId } });
+        next();
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Fail to unfollow. Please try again later.'
+        })
+    }
+}
+
+const findPeople = async (req, res, next) => {
+
+    const following = req.profile.following;
+    following.push(req.profile._id);
+
+    try {
+        const users = await User.find({ _id: { $nin: following } }).select('name');
+        return res.status(200).json(users);
+    } catch (err) {
+        return res.status(400).json({
+            error: 'Error in retrieving unfollowed users'
+        });
+    }
+}
+
+export default {
+    create,
+    list,
+    userById,
+    read,
+    update,
+    remove,
+    updatePassword,
+    photo,
+    defaultPhoto,
+    addFollower,
+    addFollowing,
+    removeFollower,
+    removeFollowing,
+    findPeople
+};
