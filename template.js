dda2cca9dccb04f2a845092dab4238fbbbccbe51
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-07-02 08:58:49 +0800

    Add index.html template

---
+++ template.js
@@ -0,0 +1,24 @@
+export default () => {
+    return `<!doctype html>
+        <html lang="en">
+            <head>
+                <meta charset="utf-8">
+                <title>MERN Skeleton</title>
+                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+                <style>
+                    a {
+                        text-decoration: None;
+                    }
+                </style>
+            </head>
+            <body>
+                <div id="root"></div>
+                <script type="text/javascript" src="/dist/bundle.js"></script>
+            </body>
+        </html>
+    `
+}
+
+{/* <style id='jss-server-side'>${css}</style> */}
+
