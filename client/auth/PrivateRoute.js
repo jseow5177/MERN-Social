fa48d4712cf353b1b049bc3a9480b9e015629775
Jonathan Seow <jseow006@e.ntu.edu.sg>
2020-08-11 20:12:52 +0800

    Minor edits

--- client/auth/PrivateRoute.js
+++ client/auth/PrivateRoute.js
@@ -3,9 +3,12 @@ import { Route, Redirect } from 'react-router-dom';
 import auth from './auth-helper';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
-    <Route {...rest} render={props => (
-        auth.isAuthenticated() ? (<Component {...props} />) : (<Redirect to={{ pathname: '/signin', state: { from: props.location } }} />)
-    )} />
+    return (
+        <Route {...rest} render={props => (
+            // state stores information about where the redirection comes from
+            auth.isAuthenticated() ? (<Component {...props} />) : (<Redirect to={{ pathname: '/signin', state: { from: props.location } }} />)
+        )} />
+    )
 }
 
 export default PrivateRoute;
